adcb3c1c9ad582e90980339786068ca40e1b0827
JOHN NASIMBA <nasimba4john@gmail.com>
2021-02-15 11:20:23 +0200

    Creating pdf json file

---
+++ data.js
@@ -0,0 +1,69 @@
+var data = [
+    {
+        title: 'Create a back-end app with Javascript skill path',
+        courseUrl: 'https://www.codecademy.com/learn/paths/create-a-back-end-app-with-javascript',       
+        provider: 'codecademy',
+        category: 'back end',
+        date: new Date('August 30, 2020'),
+        courseHours: 32,
+        languages: [
+            'JavaScript', 'Node.js', 'Express', 'APIs', 'HTTP', 'SQL', 'Node-SQLite'
+        ],      
+        pdfUrl: 'https://drive.google.com/file/d/14lpDRD6bYh1COY3_thB7RqR6auLKdT0p/preview'
+    },
+    {
+        title: 'Advance your node.js skills',
+        courseUrl: 'https://www.linkedin.com/learning/paths/advance-your-node-js-skills',
+        provider: 'Linkedin  learning',
+        category: 'back end',
+        date: new Date('June 09, 2020'),
+        courseHours: 17,
+        topics: [
+            'Node.js', 'Security', ' Design Patterns', 'Micro services',
+            'Testing and Code Quality', 'Debugging and Performance',
+            'Scaling Applications'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/1hc2g4-vTTne4LM6S0uE-MJ71jcSNan6P/preview',
+
+    },
+    {
+        title: 'Advanced Express',
+        courseUrl: 'https://www.linkedin.com/learning/advanced-express',
+        provider: 'Linkedin  learning',
+        category: 'back end',
+        date: new Date('September 05, 2020'),
+        courseHours: 2,
+        topics: [
+            'Node.js', 'Express',  'Scaling Applications'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/1X32iXbx6dFVyPsXsrRbQUJkms8Yy1yFV/preview',
+
+    },
+    {
+        title: 'Advanced Node.js',
+        courseUrl: 'https://www.linkedin.com/learning/advanced-node-js',
+        provider: 'Linkedin  learning',
+        category: 'back end',
+        date: new Date('September 05, 2020'),
+        courseHours: 2,
+        topics: [
+            'Node.js'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/1ZEFzWu4DGZ6ViEiHCJdut_tY5h0xAots/preview',
+
+    },
+    {
+        title: 'Advanced Node.js Scaling Applications',
+        courseUrl: 'https://www.linkedin.com/learning/advanced-node-js-scaling-applications',
+        provider: 'Linkedin  learning',
+        category: 'back end',
+        date: new Date('September 05, 2020'),
+        courseHours: 1,
+        topics: [
+            'Node.js', 'Garageband', 'Scaling Applications'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/1KVG_gmYTIJU9wElXuNBTwiQ-BfOJ7UTU/preview',
+
+    },
+]
+
