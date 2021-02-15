955555ed1e9a74b41fd6a82999514b14302cf6d4
JOHN NASIMBA <nasimba4john@gmail.com>
2021-02-15 12:28:50 +0200

    Adding more items

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ var data = [
         title: 'Create a back-end app with Javascript skill path',
         courseUrl: 'https://www.codecademy.com/learn/paths/create-a-back-end-app-with-javascript',       
         provider: 'codecademy',
-        category: 'back end',
+        category: 'back-end',
         date: new Date('August 30, 2020'),
         courseHours: 32,
         languages: [
@@ -15,7 +15,7 @@ var data = [
         title: 'Advance your node.js skills',
         courseUrl: 'https://www.linkedin.com/learning/paths/advance-your-node-js-skills',
         provider: 'Linkedin  learning',
-        category: 'back end',
+        category: 'back-end',
         date: new Date('June 09, 2020'),
         courseHours: 17,
         topics: [
@@ -30,7 +30,7 @@ var data = [
         title: 'Advanced Express',
         courseUrl: 'https://www.linkedin.com/learning/advanced-express',
         provider: 'Linkedin  learning',
-        category: 'back end',
+        category: 'back-end',
         date: new Date('September 05, 2020'),
         courseHours: 2,
         topics: [
@@ -43,7 +43,7 @@ var data = [
         title: 'Advanced Node.js',
         courseUrl: 'https://www.linkedin.com/learning/advanced-node-js',
         provider: 'Linkedin  learning',
-        category: 'back end',
+        category: 'back-end',
         date: new Date('September 05, 2020'),
         courseHours: 2,
         topics: [
@@ -56,7 +56,7 @@ var data = [
         title: 'Advanced Node.js Scaling Applications',
         courseUrl: 'https://www.linkedin.com/learning/advanced-node-js-scaling-applications',
         provider: 'Linkedin  learning',
-        category: 'back end',
+        category: 'back-end',
         date: new Date('September 05, 2020'),
         courseHours: 1,
         topics: [
@@ -65,5 +65,64 @@ var data = [
         pdfUrl: 'https://drive.google.com/file/d/1KVG_gmYTIJU9wElXuNBTwiQ-BfOJ7UTU/preview',
 
     },
+    {
+        title: 'Angular Essential Training',
+        courseUrl: 'https://www.linkedin.com/learning/angular-essential-training-2',
+        provider: 'Linkedin  learning',
+        category: 'front-end',
+        date: new Date('August 29, 2020'),
+        courseHours: 3,
+        topics: [
+            'AngularJS', 'Single Page Applications', 'Web Applications Development'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/1mO2GCk8ZsUXxkUIKYKa0boktQs9bMp2j/preview'
+    },
+    {
+        title: 'AWS and React: Creating Full-Stack Apps',
+        courseUrl: 'https://www.linkedin.com/learning/aws-and-react-creating-full-stack-apps',
+        provider: 'Linkedin  learning',
+        category: 'database and hosting',
+        date: new Date('August 29, 2020'),
+        courseHours: 1,
+        topics: [
+            'Full-Stack Development', 'React', 'AWS'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/1kgpDLQwG6_YzCO9DSfHFBkf75ZPRB0qW/preview'
+    },
+    {
+        title: 'AWS: Deploying and Provision',
+        courseUrl: 'https://www.linkedin.com/learning/aws-deploying-and-provisioning',
+        provider: 'Linkedin  learning',
+        category: 'database and hosting',
+        date: new Date('August 29, 2020'),
+        courseHours: 5,
+        topics: [
+            'AWS', 'Cloud Computing'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/1dyfGQLkXRm8S1JO4sUsYnvuMBvnU451q/preview'
+    },
+    {
+        title: 'AWS for Developers: RDS MySQL Database with Lambdas',
+        courseUrl: 'https://www.linkedin.com/learning/aws-for-developers-rds-mysql-database-with-lambdas',
+        provider: 'Linkedin  learning',
+        category: 'database and hosting',
+        date: new Date('August 01, 2020'),
+        courseHours: 1,
+        topics: [
+            'AWS Lambda', 'Relational Databases', 'MySQL'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/1rRJ2fmsG_L4iQ0wYBTzDfMi90V3sdzBz/preview'
+    },
+    {
+        title: 'Become a Database Developer',
+        courseUrl: 'https://www.linkedin.com/learning/paths/become-a-database-developer',
+        provider: 'Linkedin  learning',
+        category: 'database and hosting',
+        date: new Date('September 04, 2020'),
+        courseHours: 10,
+        topics: [
+            'Programming Foundations: Databases', 'SQL Programming', 'MySQL Development', 'MongoDB'
+        ],
+        pdfUrl: 'https://drive.google.com/file/d/18RWBRbAyGVPNl_t5hjGZVCTC3boYFAkf/preview'
+    },
 ]
-
