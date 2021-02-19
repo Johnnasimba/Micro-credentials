399e9f170e8c219a084eb9c3a5e37601d7b36940
JOHN NASIMBA <nasimba4john@gmail.com>
2021-02-19 12:52:46 +0200

    Adding pagination

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,22 @@
 
 var app = angular
-    .module('appModule', [])
+    .module('appModule', ['angularUtils.directives.dirPagination'])
     .config(function ($sceProvider) {
         $sceProvider.enabled(false);
     })
    
-    .controller('AppController', function ($scope, $http) {  
+    .controller('AppController', function ($scope, $http, $location,$anchorScroll) {  
         // Fetching data from static json file
         $http.get('scripts/data.json').then(function(response) {
             $scope.certificates = response.data;
-          })
-        
+          })   
         
         $scope.search = '';
-
+        
+        $scope.gotoTop = function () {
+            $location.hash('header-navbar');
+            $anchorScroll();
+        }
     });
 
 
